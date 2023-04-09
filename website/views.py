b4f52f09a1e379e2b45f638a3568c32d97b4eeec
Phan Cong Phong <phongdo.pbc@gmail.com>
2023-04-09 17:15:03 +0300

    Hoan thanh chuc nang them sua xoa co ban

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .form import SignUpForm
+from .form import SignUpForm, AddRecordForm
 from .models import Record
 
 
@@ -72,4 +72,30 @@ def delete_record(request, pk):
 
 
 def add_record(request):
-    return render(request, 'add_record.html', {})
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_rec = form.save()
+                messages.success(request, f"Record Added....")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, f"You must be logged in....")
+        return redirect('home')
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request,f'Record Has Been Updated!!!')
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(request, f"You must be logged in....")
+        return redirect('home')
+
+        
